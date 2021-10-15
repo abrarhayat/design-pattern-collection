a519dd35e9055bd8c054a4eb20cc5c05c4d38b46
Abrar Hayat <abrarhayat@gmail.com>
2021-10-16 01:28:28 +0600

    Printed out command names in Command Test

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -8,8 +8,11 @@ public class CommandTest {
     public void testCommand() {
         CommandManager commandManager = CommandManager.getInstance();
         SearchCommand searchCommand1 = new SearchCommand("Search Command 1");
+        System.out.println(searchCommand1.getCommandName());
         WriteCommand writeCommand = new WriteCommand("Write Command 1");
+        System.out.println(writeCommand.getCommandName());
         SearchCommand searchCommand2 = new SearchCommand("Search Command 2");
+        System.out.println(searchCommand2.getCommandName());
 
         commandManager.invokeCommand(searchCommand1);
         commandManager.invokeCommand(writeCommand);
